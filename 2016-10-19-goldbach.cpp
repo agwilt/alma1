2ecb048ad797e3c2f30264428f95f72962ae54a6
Andreas Gwilt <andreas@gwilt.net>
2016-10-21 15:48:24 +0200

    Nicer variable names

--- 2016-10-19-goldbach.cpp
+++ 2016-10-19-goldbach.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	int b = atoi(argv[2]);
 
 	// Error if a or b are under 4
-	if (b<4 or a<4) {
+	if (b<4 || a<4) {
 		std::cerr << "Please make sure a and b are at least 4.\n";
 		return 1;
 	}
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 	// Initialize array to store my list of primes, along with its length
 	int *primes = new int[b];
-	int *primes_backup = primes;
+	int *p = primes;
 	int p_length = 0;
 
 	// Start sieving for primes
@@ -58,11 +58,11 @@ int main(int argc, char *argv[])
 	for (int c=a; c<=b; c+=2) {
 		sums_temp = 0;
 		// Go through all primes <= c/2 and check if c-prime is prime.
-		for (; *primes <= (c/2); ++primes) {
+		for (p = primes; *p <= (c/2); ++p) {
 			// Stop if c already has as many sums as the minimum
 			if (sums_temp >= sums) break;
 			// If a prime sum was found, increment counter
-			if (is_prime[c-*primes]) ++sums_temp;
+			if (is_prime[c-*p]) ++sums_temp;
 		}
 		// Update sums and min if a "better" c was found
 		if (sums_temp < sums) {
@@ -70,15 +70,14 @@ int main(int argc, char *argv[])
 			min = c;
 		}
 		// "Reset" the primes array
-		primes = primes_backup;
 	}
 
 	// Loop to output min, sums, and all of min's sums.
 	std::cout << "Even number between " << a << " and " << b << " with the least prime sums:\n";
 	std::cout << min << " has " << sums << ":\n";
 
-	for (int i=0; primes[i]<=(min/2); ++i) {
-		int p = primes[i];
-		if (is_prime[min-p]) std::cout << p << " + " << min-p << "\n";
+	for (p = primes; *p <= (min/2); ++p) {
+		if (is_prime[min-*p])
+			std::cout << *p << " + " << min-*p << "\n";
 	}
 }
