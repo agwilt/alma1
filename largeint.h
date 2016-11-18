788231957e82405998519766f037882263669886
jay-el124 <jay-el124@users.noreply.github.com>
2016-11-18 16:28:37 +0100

    Change spaces to tabs

--- largeint.h
+++ largeint.h
@@ -1,6 +1,7 @@
 // largeint.h (Declaration of Class LargeInt)
 
 #include <vector>
+#include <iostream>
 #include <string>
 
 
@@ -8,17 +9,15 @@ class LargeInt {
 public:
 	using inputtype = long long;
 
-	LargeInt(inputtype);							// constructor
-	std::string decimal() const;					// decimal representation
-	bool operator<(const LargeInt &) const;			// comparison
-	LargeInt operator+(const LargeInt &) const;		// addition
+	LargeInt(inputtype);				// constructor
+	std::string decimal() const;			// decimal representation
+	bool operator<(const LargeInt &) const;		// comparison
+	LargeInt operator+(const LargeInt &) const;	// addition
 	const LargeInt & operator+=(const LargeInt &);	// addition
-
-	LargeInt operator*(LargeInt) const;		// multiplication
-	bool operator>(const LargeInt &) const;			// other comparison
-	const LargeInt & operator--();					// decrement
+	const LargeInt & operator*=(const LargeInt&);
+	LargeInt operator*(const LargeInt&) const;
 
 private:
-	std::vector<short> _v;  // store single digits, last digit in _v[0]
+	std::vector<short> _v;				// store single digits, last digit in _v[0]
 	static const std::string digits;
 };
