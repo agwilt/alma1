0fe14b86104eb5561693ab9d01d7e16d2902e607
Andreas Gwilt <andreas@gwilt.net>
2016-12-08 07:23:06 +0100

    Post-grading corrections

--- 2016-11-16-division.cpp
+++ 2016-11-16-division.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <iomanip>
 
 // To store a double in .number and mess with it using .data
 typedef union {
 	double number;
-	long data;
+	unsigned long data;
 } fp_hack;
 
 int main()
@@ -13,9 +14,23 @@ int main()
 
 	// Get input from user. We're supposed to take ints, but that's just stupid.
 	std::cout << "Zähler: ";
-	std::cin >> zaehler.number;
+	std::flush(std::cout);
+	if (! (std::cin >> zaehler.number)) return 1;
 	std::cout << "Nenner: ";
-	std::cin >> nenner.number;
+	std::flush(std::cout);
+	if (! (std::cin >> nenner.number)) return 1;
+
+	// Check for 0
+	if (nenner.number == 0) {
+		std::cerr << "Don't be an idiot.\n";
+		return 1;
+	}
+
+	// Bit magic spell is broken if zaehler=0
+	if (zaehler.number == 0) {
+		std::cout << 0 << std::endl;
+		return 0;
+	}
 
 	// Make sure the denominator is positive
 	if (nenner.number<0) {
@@ -23,10 +38,9 @@ int main()
 		zaehler.number *= -1;
 	}
 
-	// Bit magic. 0xc020000000000000 = K_2(1022l<<52)
 	// Diff is the exponent differenc to subtract from zaehler and nenner, so
 	// that nenner's exponent is -1
-	long diff = (nenner.data + 0xc020000000000000) & 0xfff0000000000000;
+	unsigned long diff = (nenner.data - (1022lu<<52)) & 0xfff0000000000000;
 	zaehler.data -= diff;
 	nenner.data -= diff;
 
@@ -34,8 +48,8 @@ int main()
 	inverse = 2.823529411764706 - 1.8823529411764706 * nenner.number;
 	// One or two Newton method iterations
 	for (int i=0; i<4; ++i)
-		inverse += inverse*(1-(nenner.number*inverse));
+		inverse *= 2-(nenner.number*inverse);
 
-	// Output using printf (to specify the number of digits to print)
-	printf("%.15g\n", zaehler.number*inverse);
+	// Output with 15 digits
+	std::cout << std::setprecision(15) << zaehler.number*inverse << std::endl;
 }
